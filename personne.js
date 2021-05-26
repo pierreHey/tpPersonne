ffb6dc45e6bd9fc19f4c9672e1ca3757f1177c4b
Pierre <pierre.heydon2021@campus-eni.fr>
2021-05-26 15:19:15 +0200

    maj personne.js

--- personne.js
+++ personne.js
@@ -4,8 +4,6 @@ let url = 'https://projettestfirebase-72677-default-rtdb.europe-west1.firebaseda
 
 let listePersonne = [];
 lire();
-// on va regarder si tableau != undefined => si vrai => initialise => sinon ajoute nvlle personne
-
 
 //----------------------------------------------------------------
 // ajouter
@@ -20,7 +18,7 @@ function ajouterPersonne() {
     listePersonne.push(personne); // add l'objet personne dans le tableau
 
     // console.log(listePersonne);
-    
+
     afficherHTML()
 }
 //----------------------------------------------------------------
@@ -28,6 +26,10 @@ function ajouterPersonne() {
 //----------------------------------------------------------------
 
 function afficherHTML() {
+    // on va regarder si tableau != undefined => 
+    // si vrai => initialise => sinon ajoute nvlle personne
+
+
     // je vide le tableau
     document.getElementById("liste").innerHTML = "";
     // parcours le tableau js pour ajouter <tr><td> ...
@@ -44,34 +46,25 @@ function afficherHTML() {
         let icone = document.createElement('i');
         icone.setAttribute('class', 'fa fa-trash');
         boutonSuppr.appendChild(icone);
-        td3.appendChild(boutonSuppr);
-
-        boutonSuppr.textContent = "Supprimer";
-        boutonSuppr.setAttribute("class", "rouge"); // attribut du bouton
         boutonSuppr.onclick = function () { // on définit l'action du bouton => appel de la fonction supprimerPersonne()
-            //supprimerPersonne();
-            //this.parentElement.parentElement.remove();
-            //ajouterFire();
+
 
             // j'ai besoin de l'indice afficherHTML();
-
-            let indice = this.parentElement.parentElement.rowIndex -1;
+            let indice = this.parentElement.parentElement.rowIndex - 1;
             //console.log("indice : "+indice);
             listePersonne.splice(indice, 1); // effacer 
-            afficherHTML()
-            ajouterFire()
+            afficherHTML() // maj html
+            ajouterFire() // et ajoute sur fire la liste
         }
-
-
 
         td1.textContent = personne.prenom;
         td2.textContent = personne.nom;
-
+        td3.appendChild(boutonSuppr);
         // mettre le td1 dans la balise tr
         tr.appendChild(td1);
         tr.appendChild(td2);
-
-        td2.appendChild(boutonSuppr)
+        tr.appendChild(td3);
+        //td2.appendChild(boutonSuppr)
 
         document.getElementById("liste").appendChild(tr) // <tbody><tr>...
     }
@@ -90,7 +83,6 @@ function ajouterFire() {
     param.header = { 'Content-Type': 'application/json' };
     param.body = JSON.stringify(listePersonne); // serialise le tableau en chaine de caractere
 
-
     fetch(url, param)
         .then(response => response.json())
         .then(info => {
@@ -98,7 +90,6 @@ function ajouterFire() {
 
         });
 }
-
 
 function lire() {
 
@@ -114,61 +105,12 @@ function lire() {
 }
 
 
-function supprimerPersonne() {
-
-    // pour l'index de la personne dans la liste on va supprimer la personne qui correspond a son index
-
-    // => on reprend la liste et on supprime la personne et on le re-put la liste
-
-
-    //document.getElementById("liste").deleteRow(##### recupere index de la ligne dans son tableau)
-
-
-
-
-
-
-    /**  
-        for (let idListe in listePersonne) {
-        //if(idListe == listePersonne.indexOf()){
-            console.log("listePersonne.indexOf() : " + idListe);
-        //}
-      
-      
-      for (let idDansLaListe in listePersonne) { // in => recupere index
-         if(idDansLaListe = listePersonne.indexOf()){
-             console.log(idDansLaListe);
-             console.log("listePersonne.indexOf()"+listePersonne.indexOf());
-         }
-       
-     }
-     */
-
-
-
-
-    let param = {}
-    param.method = 'DELETE'
-    param.header = { 'Content-Type': 'application/json' };
-    param.body = JSON.stringify(listePersonne); // serialise le tableau en chaine de caractere
-
-
-    fetch(url, param)
-        .then(response => response.json())
-        .then(info => {
-            console.log(info);
-
-        });
-
-}
-
-
 //  TP 3 déployer sur Git et sur Firebase hosting :
-//  Ajouter un répertoire docs et venir bloquer avec .gitingore
+//  Ajouter un répertoire docs et venir bloquer avec .gitingore // 
 
-//	Ajouter  une branche develop à votre repository et merger cette branche
+//	Ajouter  une branche develop à votre repository et merger cette branche // 
 
-//  Ajouter la possibilité d'enlever une personne de la liste et faire un commit & un push
-//  Bonus utiliser fontawesome
+//  Ajouter la possibilité d'enlever une personne de la liste et faire un commit & un push // 
+//  Bonus utiliser fontawesome // 
 //  Lien git
 
