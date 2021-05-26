705bdd9c8c261945359f20661f54a009055c1e29
Pierre <pierre.heydon2021@campus-eni.fr>
2021-05-26 12:38:20 +0200

    commit avant changement branche

--- personne.js
+++ personne.js
@@ -35,11 +35,20 @@ function afficherHTML() {
         let tr = document.createElement("tr"); // créer balise <tr> </tr>
         let td1 = document.createElement("td"); // créer balise <td> </td>
         let td2 = document.createElement("td");
+       let boutonSuppr = document.createElement("button")
+      // boutonSuppr.onclick = supprimerPersonne();
+        boutonSuppr.textContent="Supprimer";
+        boutonSuppr.setAttribute("class","rouge");
+
         td1.textContent = personne.prenom;
         td2.textContent = personne.nom;
+        
         // mettre le td1 dans la balise tr
         tr.appendChild(td1);
         tr.appendChild(td2);
+        
+        tr.appendChild(boutonSuppr)
+        
         document.getElementById("liste").appendChild(tr) // <tbody><tr>...
     }
     ajouterFire();
@@ -80,9 +89,29 @@ function lire() {
 
 }
 
+
+function supprimerPersonne()  {
+    let param = {}
+    param.method = 'DELETE'
+    param.header = { 'Content-Type': 'application/json' };
+    param.body = JSON.stringify(listePersonne); // serialise le tableau en chaine de caractere
+
+
+    fetch(url, param)
+        .then(response => response.json())
+        .then(info => {
+            console.log(info);
+
+        });
+
+}
+
+
 //  TP 3 déployer sur Git et sur Firebase hosting :
 //  Ajouter un répertoire docs et venir bloquer avec .gitingore
+
 //	Ajouter  une branche develop à votre repository et merger cette branche
+
 //  Ajouter la possibilité d'enlever une personne de la liste et faire un commit & un push
 //  Bonus utiliser fontawesome
 //  Lien git
