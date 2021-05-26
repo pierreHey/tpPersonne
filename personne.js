e5dbb515c1411022701083c47b8e03655d23d09a
Pierre <pierre.heydon2021@campus-eni.fr>
2021-05-26 10:19:53 +0200

    Envoi des données sur firebase

--- personne.js
+++ personne.js
@@ -3,6 +3,7 @@ let url = 'https://projettestfirebase-72677-default-rtdb.europe-west1.firebaseda
 
 
 let listePersonne = [];
+lire();
 // on va regarder si tableau != undefined => si vrai => initialise => sinon ajoute nvlle personne
 
 
@@ -15,11 +16,42 @@ function ajouterPersonne() {
     let personne = {};
     personne.nom = nom;
     personne.prenom = prenom;
-    
+
     listePersonne.push(personne); // add l'objet personne dans le tableau
 
     console.log(listePersonne);
+    afficherHTML()
+}
+//----------------------------------------------------------------
+// afficher
+//----------------------------------------------------------------
 
+function afficherHTML() {
+    // je vide le tableau
+    document.getElementById("liste").innerHTML = "";
+    // parcours le tableau js pour ajouter <tr><td> ...
+    for (let personne of listePersonne) {
+
+        let tr = document.createElement("tr"); // créer balise <tr> </tr>
+        let td1 = document.createElement("td"); // créer balise <td> </td>
+        let td2 = document.createElement("td");
+        td1.textContent = personne.prenom;
+        td2.textContent = personne.nom;
+        // mettre le td1 dans la balise tr
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        document.getElementById("liste").appendChild(tr) // <tbody><tr>...
+    }
+    ajouterFire();
+}
+
+
+//----------------------------------------------------------------
+// ajouter dans firebase
+//----------------------------------------------------------------
+
+
+function ajouterFire() {
     let param = {}
     param.method = 'PUT'
     param.header = { 'Content-Type': 'application/json' };
@@ -32,80 +64,18 @@ function ajouterPersonne() {
             console.log(info);
 
         });
-
-
-        afficherHTML()
 }
-//----------------------------------------------------------------
-// afficher
-//----------------------------------------------------------------
-
-function afficherHTML()
-{
-    // je vide le tableau
-    document.getElementById("liste").innerHTML="";
-    // parcours le tableau js pour ajouter <tr><td> ...
-    for(let personne of listePersonne){
-        
-        let tr = document.createElement("tr"); // créer balise <tr> </tr>
-        let td1 = document.createElement("td"); // créer balise <td> </td>
-        let td2 =document.createElement("td");
-        td1.textContent = personne.prenom;
-        td2.textContent = personne.nom;
-        // mettre le td1 dans la balise tr
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        document.getElementById("liste").appendChild(tr) // <tbody><tr>...
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function lire() {
 
     fetch(url)
         .then(response => response.json())
-        .then(info => {
-            console.log(info);
+        .then(tab => {
+            if (tab != undefined) {
+                listePersonne = tab;
+                afficherHTML();
+            }
+        })
 
-        });
-
-  console.log("function lire() =>>>>>>"+lire22());
-  
 }
-
-
-
-
-async function lire22() {
-    const response = await fetch(url, {});
-    const json = await response.json();
-
-    
-    return json;
-}
-
-
-
-
-
-
-
